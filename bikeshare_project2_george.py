05719a960d52cb363303ec1c05bbf480617b1a9d
Simon Bao <xbao_2000@yahoo.com>
2022-07-24 22:40:14 +1200

    Delete comment

--- bikeshare_project2_george.py
+++ bikeshare_project2_george.py
@@ -122,16 +122,7 @@ def get_filters():
 
 # create this function to loads data for the specified city and filters by month and day if applicable.
 def load_data(city, month, day):
-    """
-    Loads data for the specified city and filters by month and day if applicable.
 
-    Args:
-        (str) city - name of the city to analyze
-        (str) month - name of the month to filter by, or "all" to apply no month filter
-        (str) day - name of the day of week to filter by, or "all" to apply no day filter
-    Returns:
-        df - Pandas DataFrame containing city data filtered by month and day
-    """
     # load data file into a dataframe
     df = pd.read_csv(CITY_DATA[city])
 
